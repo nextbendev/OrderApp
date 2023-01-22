6aa8319fd0f9878e267af34022459502c5e323b9
Ben <nextbendev@gmail.com>
2023-01-22 11:48:45 -0500

    find error

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,26 +1,40 @@
+import { useContext } from 'react';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 
 
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext);
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.length > 0;
+    const cartItemRemoveHandler = (id) => {};
+    const cartItemAddHandler = (item) => {};
     const cartItems = (
         <ul className={classes['cart-items']}>
-            {[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map((item) => (
-            <li>{item.name}</li>
+            {cartCtx.items.map((item) => (
+            <CartItem
+                key={item.id}
+                name={item.name}
+                amount={item.amount}
+                price={item.price}
+                onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                onAdd={cartItemAddHandler} />
         ))}
         </ul>
     );
 
     return(
+        
 
         <Modal 
-            onClose={props.onClose}
-        >
+            onClose={props.onClose}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total</span>
-                <span>$69</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button 
@@ -29,8 +43,7 @@ const Cart = (props) => {
                     onClick={props.onClose}
                     >Close
                 </button>
-                <button className={classes.button}>Order</button>
-
+                {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     );
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,6 @@
 import {Fragment} from "react";
 import HeaderCartButton from './HeaderCartButton';
-import mealImg from '../../assets/meals.jpeg';
+import mealImg from '../../assets/crypto.jpeg';
 import classes from './Header.module.css';
 
 
@@ -8,7 +8,7 @@ const Header = (props) => {
     return ( 
         <Fragment>
             <header className={classes.header}>
-                <h1>React Meals</h1>
+                <h1>CryptoBroski's Exchange</h1>
                 <HeaderCartButton onClick={props.onShowCart}/>
             </header>
             <div className={classes['main-image']}>
